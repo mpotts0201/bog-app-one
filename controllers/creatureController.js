d67634c8144f00490fb1a2788b8acda5f31bae08
mpotts0201 <mpotts0201@gmail.com>
2018-03-08 12:58:05 -0500

    built route to send data

--- controllers/creatureController.js
+++ controllers/creatureController.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router({ mergeParams: true })
+const { Creature } = require('../db/schema')
+
 
 router.get('/', (req, res) => {
-    res.send("Creature Comforts")
+    Creature.find().then((creatures) => {
+        res.send(creatures)
+    }).catch((err) => {
+        console.log(err)
+    })
 })
 
 
